caf772c933ee12620d98b83e2352061053bf354f
jhonlz09 <jhonlz2011@hotmail.com>
2026-08-20 22:24:04 -0500

    feat: setup melany-davila cv and github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-const SERVER_PORT = 3000;
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://Jhonlz09.github.io";
-
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-
-let BASE_URL = LOCALHOST_URL;
-
-if(isBuild){
-    BASE_URL = LIVE_URL;
-}
-
 // https://astro.build/config
 export default defineConfig({
-    site: BASE_URL,
-    
+  site: 'https://melany-davila.github.io',
+  base: '/melany-davila' // Si tu repositorio se llama melany-davila. (Si fuera melany-davila.github.io, puedes omitir 'base')
 });
+
